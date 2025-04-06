e3fdf0b3ad5169db1b9083e517848f28023a4a87
sadafsayeed <121372300+sadafsayeed@users.noreply.github.com>
2025-04-05 22:30:17 -0400

    Fixed issues for phones

--- app/(tabs)/calendar.tsx
+++ app/(tabs)/calendar.tsx
@@ -1,27 +1,23 @@
 // app/calendar.tsx
 import React from "react";
-import {useRouter} from "expo-router";
-import {Button} from "react-native";
+import { useRouter } from "expo-router";
+import { Button, View } from "react-native";
 import CalendarScreen from '../../components/CalendarScreen';
 
-export default function Calendar(){
-    const router = useRouter();
-    const handleClick = ()=>{
-        router.push("/VoiceToText");
-    }
-    return(
-        <div>
-            <CalendarScreen/>
-            <div className="VTS Button">
-                <Button
-                title = "VTS"
-                onPress={handleClick}
-                />
-            </div>
-        </div>
-        
-    )
-}
+export default function Calendar() {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push("/VoiceToText");
+  };
 
-// Button for voice-to-text
-// When user presses the button, take them to a new screen and do voice-to-text
+  return (
+    <View>
+      <View style={{ padding: 20 }}>
+        <CalendarScreen />
+      </View>
+      <View style={{ marginTop: 20 }}>
+        <Button title="VTS" onPress={handleClick} />
+      </View>
+    </View>
+  );
+}
