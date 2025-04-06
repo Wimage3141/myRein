8d544a7a38fe0a5034779375b3f584ad494b407a
sadafsayeed <121372300+sadafsayeed@users.noreply.github.com>
2025-04-05 21:50:47 -0400

    Updated structure

--- app/(tabs)/calendar.tsx
+++ app/(tabs)/calendar.tsx
@@ -1,3 +1,27 @@
 // app/calendar.tsx
-import CalendarScreen from '../../CalendarScreen';
-export default CalendarScreen;
+import React from "react";
+import {useRouter} from "expo-router";
+import {Button} from "react-native";
+import CalendarScreen from '../../components/CalendarScreen';
+
+export default function Calendar(){
+    const router = useRouter();
+    const handleClick = ()=>{
+        router.push("/VoiceToText");
+    }
+    return(
+        <div>
+            <CalendarScreen/>
+            <div className="VTS Button">
+                <Button
+                title = "VTS"
+                onPress={handleClick}
+                />
+            </div>
+        </div>
+        
+    )
+}
+
+// Button for voice-to-text
+// When user presses the button, take them to a new screen and do voice-to-text
---
+++ app/VoiceToText.tsx
@@ -0,0 +1,14 @@
+
+export default function VTS(){
+    return(
+        <div className="Voice-Text" >
+            <div className="Voice">
+                <h1>This will take the voice</h1>
+                
+            </div>
+            <div className="Text">
+                <h1>This will display what the user said</h1>
+            </div>
+        </div>
+    )
+}
