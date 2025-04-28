587df348d22aaf63e9babca79891a1dd5e75314f
Wimage03 <nafi314159@gmail.com>
2025-04-27 21:36:22 -0400

    implemented the tabs and basic skeleton

---
+++ Components/Calendar.tsx
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Calendar = () => {
+
+  const tasks = [
+    {id: "task-1", startTime: ""}, 
+  ]
+
+  return (
+    <View>
+      <Text>Calendar</Text>
+    </View>
+  )
+}
+
+export default Calendar
---
+++ app/types.ts
@@ -0,0 +1,9 @@
+type  Task = {
+    id: string;
+    taskDesc: string;
+    date: string;
+    startTime: string;
+    endTime: string;
+    timeStarted: string;
+    timeEnded: string;
+}
